2048e7dfb6875afdad256a02edfc5724f1d8135d
Sierra Ellison <sellison111@gmail.com>
2020-12-30 21:14:24 -0500

    pull todos using TodoService from jsonplaceholder

--- angular-todolist/src/app/app.module.ts
+++ angular-todolist/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
 
   //services we want to include
--- angular-todolist/src/app/components/todos/todos.component.ts
+++ angular-todolist/src/app/components/todos/todos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TodoService} from '../../services/todo.service'
 import { Todo } from '../../models/Todo';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -9,26 +11,12 @@ export class TodosComponent implements OnInit {
   todos:Todo[];
 
   //used to import services
-  constructor() { }
+  constructor(private todoServce:TodoService ) { }
 
-  ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ]
+  ngOnInit() {
+    this.todoServce.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
   }
 
 }
