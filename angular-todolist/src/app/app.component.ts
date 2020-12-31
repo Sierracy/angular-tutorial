7a5f61f67e058fe9eee38faa83194a8fa3d2a91a
Sierra Ellison <sellison111@gmail.com>
2020-12-30 19:37:37 -0500

    demonstrate use of pipe, constructor, methods and string interpolation

--- angular-todolist/src/app/app.component.ts
+++ angular-todolist/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+//The decorator includes metadata for the component
 @Component({
+  //the selector is used in the index.html
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todolist';
+  //define as a string in typescript
+  title:string = 'Todo List';
+
+  constructor(){
+    this.changeTitle('Doto List')
+  }
+
+  changeTitle(title:string):void {
+    this.title = title;
+  }
 }
--- angular-todolist/src/app/app.module.ts
+++ angular-todolist/src/app/app.module.ts
@@ -4,7 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//The root app module
 @NgModule({
+
+  //our new components are added here
+  //using the cli automatically adds components to declarations
   declarations: [
     AppComponent
   ],
@@ -12,6 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
+
+  //services we want to include
   providers: [],
   bootstrap: [AppComponent]
 })
